afdb402fb9decf8483cd714dd54d7750a3ee04c1
zestyasuo <zyasuo@student.21-school.ru>
2024-01-03 01:17:44 +0300

    two triangles with different color

--- engine_tester/src/main.rs
+++ engine_tester/src/main.rs
@@ -2,6 +2,7 @@ use std::{mem, ptr};
 use gl::types::*;
 
 use rustgldrawer::graphics::{window::Window, glwrapper::*};
+
 fn main() {
 	let mut win = Window::new(1080, 720, "gl");
 	win.init_gl();
@@ -11,23 +12,32 @@ fn main() {
 		"../src/graphics/shaders/frag.glsl",
 	);
 
-	let vertices: [f32; 12] = [
-		0.5, 0.5, 0.0, 0.5, -0.5, 0.0, -0.5, -0.5, 0.0, -0.5, 0.5, 0.0,
+	let sp2 = ShaderProgram::new(
+		"../src/graphics/shaders/vertex.glsl",
+		"../src/graphics/shaders/frag2.glsl",
+	);
+
+	let triangle1: [f32; 9] = [
+		0.0, 0.5, 0.0,
+		0.0, 0.3, 0.0,
+		-0.5, 0.0, 0.0,
 	];
 
-	let indexes = [0, 1, 3, 1, 2, 3];
+	let triangle2: [f32; 9] = [
+		0.0, -0.3, 0.0,
+		0.0, -0.5, 0.0,
+		0.5, 0.0, 0.0,
+	];
 
-	let vao = Vao::new();
-	vao.bind();
+	// let indexes = [0, 1, 3, 1, 2, 3];
 
-	let vbo = BufferObject::new(gl::ARRAY_BUFFER, gl::STATIC_DRAW);
-	vbo.bind();
-	vbo.store_f32_data(&vertices);
+	let vao1 = Vao::new();
+	vao1.bind();
+	let vbo_tr1 = BufferObject::new(gl::ARRAY_BUFFER, gl::STATIC_DRAW);
+	vbo_tr1.bind();
+	vbo_tr1.store_f32_data(&triangle1);
 
-	let ebo = BufferObject::new(gl::ELEMENT_ARRAY_BUFFER, gl::STATIC_DRAW);
-	ebo.bind();
-	ebo.store_i32_data(&indexes);
-	let position_attribute = VertexAttribute::new(
+	let pos_atrr1 = VertexAttribute::new(
 		0,
 		3,
 		gl::FLOAT,
@@ -35,26 +45,48 @@ fn main() {
 		3 * mem::size_of::<GLfloat>() as GLsizei,
 		ptr::null(),
 	);
+	pos_atrr1.enable();
 
-	let index_attribute = VertexAttribute::new(
-		1,
+	let vao2 = Vao::new();
+	vao2.bind();
+	let vbo_tr2 = BufferObject::new(gl::ARRAY_BUFFER, gl::STATIC_DRAW);
+	vbo_tr2.bind();
+	vbo_tr2.store_f32_data(&triangle2);
+
+	let pos_attr2 = VertexAttribute::new(
+		0,
 		3,
 		gl::FLOAT,
 		gl::FALSE,
 		3 * mem::size_of::<GLfloat>() as GLsizei,
 		ptr::null(),
 	);
+	pos_attr2.enable();
+	// let ebo = BufferObject::new(gl::ELEMENT_ARRAY_BUFFER, gl::STATIC_DRAW);
+	// ebo.bind();
+	// ebo.store_i32_data(&indexes);
+	// let index_attribute = VertexAttribute::new(
+	// 	1,
+	// 	3,
+	// 	gl::FLOAT,
+	// 	gl::FALSE,
+	// 	3 * mem::size_of::<GLfloat>() as GLsizei,
+	// 	ptr::null(),
+	// );
 
-	position_attribute.enable();
-	index_attribute.enable();
+	// index_attribute.enable();
 
 	while !win.should_close() {
 		unsafe {
 			gl::ClearColor(0.2, 0.3, 0.3, 1.0);
 			gl::Clear(gl::COLOR_BUFFER_BIT);
 			sp.bind();
-			vao.bind();
-			gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, ptr::null());
+			vao1.bind();
+			gl::DrawArrays(gl::TRIANGLES, 0, 3);
+			sp2.bind();
+			vao2.bind();
+			gl::DrawArrays(gl::TRIANGLES, 0, 3);
+			// gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, ptr::null());
 		}
 		win.update();
 	}
